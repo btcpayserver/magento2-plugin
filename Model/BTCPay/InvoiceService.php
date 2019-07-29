4e2bcc6fa92da2bda2527fea6258479e43950fd2
Wouter Samaey <wouter.samaey@storefront.be>
2019-07-29 12:27:48 +0200

    Cleanup

--- Model/BTCPay/InvoiceService.php
+++ Model/BTCPay/InvoiceService.php
@@ -173,41 +173,18 @@ class InvoiceService {
         return $token;
     }
 
+    /**
+     * @param \Magento\Sales\Model\Order $order
+     * @return \BitPay\Invoice
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
     public function createInvoice(\Magento\Sales\Model\Order $order) {
         $storeId = $order->getStoreId();
         $orderId = $order->getId();
 
         $client = $this->getClient($storeId);
 
-
-//            /**
-//             * The code will throw an exception if anything goes wrong, if you did not
-//             * change the $pairingCode value or if you are trying to use a pairing
-//             * code that has already been used, you will get an exception. It was
-//             * decided that it makes more sense to allow your application to handle
-//             * this exception since each app is different and has different requirements.
-//             */
-//            echo "Exception occured: " . $e->getMessage().PHP_EOL;
-//
-//            echo "Pairing failed. Please check whether you're trying to pair a production pairing code on test.".PHP_EOL;
-//            $request  = $client->getRequest();
-//            $response = $client->getResponse();
-//            /**
-//             * You can use the entire request/response to help figure out what went
-//             * wrong, but for right now, we will just var_dump them.
-//             */
-//            echo (string) $request.PHP_EOL.PHP_EOL.PHP_EOL;
-//            echo (string) $response.PHP_EOL.PHP_EOL;
-//            /**
-//             * NOTE: The `(string)` is include so that the objects are converted to a
-//             *       user friendly string.
-//             */
-
-        /**
-         * This is where we will start to create an Invoice object, make sure to check
-         * the InvoiceInterface for methods that you can use.
-         */
-        $invoice = new \BitPay\Invoice();
+        $btcpayInvoice = new \BitPay\Invoice();
 
         $ba = $order->getBillingAddress();
 
@@ -223,21 +200,21 @@ class InvoiceService {
         $buyer->setZip($ba->getPostcode());
         $buyer->setEmail($order->getCustomerEmail());
 
-        // TODO what does this notify field to exactly? Why is there a field for it on the Buyer and why is it also on the Invoice object?
+        // TODO what does this notify field to exactly? BTCPay never emails customers directly.
         $buyer->setNotify(true);
 
         // Add the buyers info to invoice
-        $invoice->setBuyer($buyer);
+        $btcpayInvoice->setBuyer($buyer);
 
         $item = new \BitPay\Item();
         $item->setCode($order->getIncrementId());
-        // TODO the descirption "Order #%1" is hard coded and not in the locale of the customer.
+        // TODO the description "Order #%1" is hard coded and not in the locale of the customer.
         $item->setDescription('Order #' . $order->getIncrementId());
         $item->setPrice($order->getGrandTotal());
         $item->setQuantity(1);
         $item->setPhysical(!$order->getIsVirtual());
 
-        $invoice->setItem($item);
+        $btcpayInvoice->setItem($item);
 
         /**
          * BTCPayServer supports multiple different currencies. Most shopping cart applications
@@ -247,17 +224,17 @@ class InvoiceService {
          *
          * @see https://docs.btcpayserver.org/faq-and-common-issues/faq-general#which-cryptocurrencies-are-supported-in-btcpay for supported currencies
          */
-        $invoice->setCurrency(new \BitPay\Currency($order->getOrderCurrencyCode()));
+        $btcpayInvoice->setCurrency(new \BitPay\Currency($order->getOrderCurrencyCode()));
 
         // Configure the rest of the invoice
         $ipnUrl = $this->storeManager->getStore()->getBaseUrl() . 'rest/V1/btcpay/ipn';
 
-        $invoice->setOrderId($order->getIncrementId());
-        $invoice->setNotificationUrl($ipnUrl);
+        $btcpayInvoice->setOrderId($order->getIncrementId());
+        $btcpayInvoice->setNotificationUrl($ipnUrl);
 
-        // TODO what do the "notification email" and "extended notifications" fields to exactly?
-        $invoice->setNotificationEmail($order->getCustomerEmail());
-        $invoice->setExtendedNotifications(true);
+        // TODO what does this notify field to exactly? BTCPay never emails customers directly.
+        //$invoice->setNotificationEmail();
+        $btcpayInvoice->setExtendedNotifications(true);
 
         /**
          * Updates invoice with new information such as the invoice id and the URL where
@@ -265,112 +242,16 @@ class InvoiceService {
          */
 
         echo 'Creating invoice at BTCPayServer now.' . PHP_EOL;
-        $client->createInvoice($invoice);
+        $client->createInvoice($btcpayInvoice);
 
-//            echo "Exception occured: " . $e->getMessage() . PHP_EOL;
-//            $request = $client->getRequest();
-//            $response = $client->getResponse();
-//            echo (string)$request . PHP_EOL . PHP_EOL . PHP_EOL;
-//            echo (string)$response . PHP_EOL . PHP_EOL;
-//            exit(1); // We do not want to continue if something went wrong
-
-        $table_name = $this->db->getTableName('btcpay_transactions');
-        $this->db->insert($table_name, [
+        $tableName = $this->db->getTableName('btcpay_transactions');
+        $this->db->insert($tableName, [
             'order_id' => $orderId,
-            'transaction_id' => $invoice->getId(),
+            'transaction_id' => $btcpayInvoice->getId(),
             'status' => 'new'
         ]);
 
-        echo 'Invoice "' . $invoice->getId() . '" created, see ' . $invoice->getUrl() . PHP_EOL;
-        echo 'Verbose details.' . PHP_EOL;
-        print_r($invoice);
-
-//        //create an item, should be passed as an object'
-//        $params = [];
-//        //$params->extension_version = $this->getExtensionVersion();
-//        $params['price'] = $order->getGrandTotal();
-//        $params['currency'] = $order->getOrderCurrencyCode();
-//
-//        $buyerInfo = [];
-//
-//        $nameParts = [];
-//        $billingAddress = $order->getBillingAddress();
-//
-//        if ($billingAddress->getFirstname()) {
-//            $nameParts[] = $billingAddress->getFirstname();
-//        }
-//        if ($billingAddress->getMiddlename()) {
-//            $nameParts[] = $billingAddress->getMiddlename();
-//        }
-//        if ($billingAddress->getLastname()) {
-//            $nameParts[] = $billingAddress->getLastname();
-//        }
-//
-//        $buyerInfo['name'] = implode(' ', $nameParts);
-//        $buyerInfo['email'] = $order->getCustomerEmail();
-//
-//        $params['buyer'] = $buyerInfo;
-//        $params['orderId'] = $orderIncrementId;
-//
-//        if ($order->getCustomerId()) {
-//            // Customer is logged in...
-//            $params['redirectURL'] = $this->url->getUrl('sales/order/view/', ['order_id' => $orderId]);
-//        } else {
-//            // Send the guest back to the order/returns page to lookup
-//            $params['redirectURL'] = $this->url->getUrl('sales/guest/form');
-//        }
-//
-//        // TODO build the URL to the REST API in a more Magento way?
-//        $params['notificationURL'] = $this->storeManager->getStore()->getBaseUrl() . 'rest/V1/btcpay/ipn';
-//        $params['extendedNotifications'] = true;
-//        $params['acceptanceWindow'] = 1200000;
-//
-//        $params['cartFix'] = $this->url->getUrl('btcpay/cart/restore', ['order_id' => $orderId]);
-//        $params['token'] = $token;
-//
-//        $postData = json_encode($params);
-//
-////        $request_headers = [];
-////        $request_headers[] = 'Content-Type: application/json';
-//
-//        $url = $this->getInvoicesEndpoint($storeId);
-//
-//        $client = $this->httpClientFactory->create();
-//        $client->setUri($url);
-//        $client->setMethod('POST');
-//        $client->setHeaders('Content-Type', 'application/json');
-//        $client->setHeaders('Accept', 'application/json');
-//        $client->setRawData($postData);
-//        $response = $client->request();
-//
-////        $ch = curl_init();
-////        curl_setopt($ch, CURLOPT_URL, $url);
-////        curl_setopt($ch, CURLOPT_HTTPHEADER, $request_headers);
-////        curl_setopt($ch, CURLOPT_POST, 1);
-////        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
-////        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-////        $result = curl_exec($ch);
-////        curl_close($ch);
-//
-//        $status = $response->getStatus();
-//        $body = $response->getBody();
-//
-//        if ($status === 200) {
-//            $data = json_decode($body, true);
-//            $invoice = new Invoice($data);
-//
-//            $table_name = $this->db->getTableName('btcpay_transactions');
-//            $this->db->insert($table_name, [
-//                'order_id' => $orderId,
-//                'transaction_id' => $invoice->getInvoiceId(),
-//                'transaction_status' => 'new'
-//            ]);
-//
-//            return $invoice;
-//        } else {
-//            // TODO improve error message
-//            throw new \RuntimeException('Cannot create new invoice in BTCPay server for order ID ' . $order->getId());
-//        }
+        return $btcpayInvoice;
     }
 
 //    public function getInvoiceURL() {
