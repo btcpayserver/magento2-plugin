df0caf226f9775fa3528c4c9ad808265ab764069
Judith <judithdemets@gmail.com>
2021-09-14 16:53:58 +0200

    comes within the changes of version 2.0.0

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -65,9 +65,8 @@ class UpgradeSchema implements UpgradeSchemaInterface
 
             // Update invoice status. Since the v1, they now start with a capital letter.
             $setup->getConnection()->query('update btcpay_invoices set status = CONCAT(UPPER(SUBSTRING(status,1,1)),LOWER(SUBSTRING(status,2)))');
-        }
 
-        if (version_compare($context->getVersion(), "3.0.0", "<")) {
+            // Add new order statuses and assign them to states
             $this->addNewStatusToState(Order::STATE_PAYMENT_REVIEW, ['status' => OrderStatuses::STATUS_CODE_FEE_TOO_LOW, 'label' => OrderStatuses::STATUS_LABEL_FEE_TOO_LOW]);
             $this->addNewStatusToState(Order::STATE_PAYMENT_REVIEW, ['status' => OrderStatuses::STATUS_CODE_UNDERPAID, 'label' => OrderStatuses::STATUS_LABEL_UNDERPAID]);
             $this->addNewStatusToState(Order::STATE_PROCESSING, ['status' => OrderStatuses::STATUS_CODE_PAID_CORRECTLY, 'label' => OrderStatuses::STATUS_LABEL_PAID_CORRECTLY]);
