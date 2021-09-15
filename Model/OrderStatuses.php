9c1f350314c744fc6d14ae6866dd01d6184f5cd1
Judith <judithdemets@gmail.com>
2021-09-15 15:22:49 +0200

    updated statuses

--- Model/OrderStatuses.php
+++ Model/OrderStatuses.php
@@ -7,13 +7,13 @@ namespace Storefront\BTCPay\Model;
 class OrderStatuses extends \Magento\Framework\Model\AbstractModel
 {
 
-    const STATUS_CODE_FEE_TOO_LOW = 'btcpay_fee_too_low';
-    const STATUS_LABEL_FEE_TOO_LOW = 'Waiting for payment confirmation';
+    const STATUS_CODE_INVALID = 'btcpay_invalid';
+    const STATUS_LABEL_INVALID = 'Waiting for payment confirmation';
 
     const STATUS_CODE_UNDERPAID = 'btcpay_underpaid';
     const STATUS_LABEL_UNDERPAID = 'Underpaid with BTCPay';
 
-    const STATUS_CODE_PAID_CORRECTLY = 'btcpay_paid_correclty';
+    const STATUS_CODE_PAID_CORRECTLY = 'btcpay_paid_correctly';
     const STATUS_LABEL_PAID_CORRECTLY = 'Paid using BTCPay';
 
     const STATUS_CODE_OVERPAID = 'btcpay_overpaid';
@@ -22,9 +22,5 @@ class OrderStatuses extends \Magento\Framework\Model\AbstractModel
     const STATUS_CODE_PENDING_PAYMENT ='btcpay_pending_payment';
     const STATUS_LABEL_PENDING_PAYMENT ='Pending BTCPay payment';
 
-    public function __construct(Context $context, \Magento\Framework\Registry $registry, ResourceModel\AbstractResource $resource = null, \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null, array $data = [])
-    {
-        parent::__construct($context, $registry, $resource, $resourceCollection, $data);
-    }
 
 }
