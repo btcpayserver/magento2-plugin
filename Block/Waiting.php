ad62218753d712474638fc8e1a170780a48ff8d7
Judith <judithdemets@gmail.com>
2021-09-21 13:55:28 +0200

    removed unused block, controller, layout + template

--- Block/Waiting.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace Storefront\BTCPay\Block;
-
-class Waiting extends \Magento\Framework\View\Element\Template
-{
-    protected $_template = 'Storefront_BTCPay::waiting.phtml';
-}
